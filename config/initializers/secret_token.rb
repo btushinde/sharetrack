13392f12f58113be6e2148ac75d381b0f9109648
Biko Tushinde <biko@smashingboxes.com>
2013-03-08 12:38:32 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharetrack::Application.config.secret_token = 'cdc2ae122b9a79ece7b07a8bbfd1169b0d01067b8181f28519692b3242c3b5f3c4bbcde7c2b69b0de008dc42fcd0884fac621cd06bf38076b39951bfb364528f'
